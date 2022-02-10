e4e433cedd5feb29f186f5d348308e0e49840d6e
Anirban Pan <pan.anirban97@gmail.com>
2022-02-10 19:45:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [weatherDetails, getWeatherDetails] = useState("");
+
+  // const appId = process.env.APP_ID;
+
+  const url = "https://api.openweathermap.org/data/2.5/forecast";
+
+  useEffect(() => {
+    getAllWeatherDetails();
+  }, []);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  const getAllWeatherDetails = () => {
+    axios
+      .get(`${url}`, {
+        params: {
+          lat: 22.6479701,
+          lon: 88.430686,
+          appid: "%env(APP_ID)%",
+        },
+      })
+      .then((response) => {
+        const allWeatherDetails = response.data.list;
+        console.log(allWeatherDetails);
+        getWeatherDetails(allWeatherDetails);
+      });
+  };
+  // getLocation();
+
+  // useEffect(() => {
+  //   if (!lat || !lng) return;
+  //   getAllWeatherDetails();
+  // }, [lat, lng]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getLocation}>Get Location</button>
+      <h1>Coordinates</h1>
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lng && <p>Longitude: {lng}</p>}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/weatherReports.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const weatherReports = () => {
+  return <div>weatherReports</div>;
+};
+
+export default weatherReports;
