8801f43d61b34d4bf3b10a730d63da02d6a29ed3
Anirban Pan <pan.anirban97@gmail.com>
2022-02-14 19:08:10 +0530

    API data fetch with complete UI

--- src/Forecast.js
+++ src/Forecast.js
@@ -35,7 +35,7 @@ const Forecast = ({ weatherDetails }) => {
 
   return (
     <div>
-      <div className="jumbotron jumbotron-fluid bg-light mb-5 mt-4">
+      <div className="jumbotron jumbotron-fluid mb-5 mt-4 ">
         <div className="container">
           <h1 className="display-4 fw-bold">5-Day Forecast</h1>
           <p className="lead"></p>
@@ -53,15 +53,50 @@ const Forecast = ({ weatherDetails }) => {
                 {weatherDetails.list[0].dt_txt}
               </small>
             </p>
+            <p className="card-text">
+              <small className="">
+                Sunrise :{" "}
+                {new Date(weatherDetails.city.sunrise).toLocaleTimeString()}
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Sunset :{" "}
+                {new Date(weatherDetails.city.sunset).toLocaleTimeString()}
+              </small>
+            </p>
             <h2 className="">
-              {weatherDetails.list[0].weather.icon}
-              <TiWeatherStormy />
+              <img
+                src={`http://openweathermap.org/img/w/${weatherDetails.list[0].weather[0].icon}.png`}
+                alt="wthr img"
+              />
+
+              {/* <TiWeatherStormy /> */}
             </h2>
             <h3>{(weatherDetails.list[0].main.temp - 273.15).toFixed(2)} °C</h3>
+            <p className="minValues" id="day2Min">
+              {(weatherDetails.list[0].main.temp_min - 273.15).toFixed(2)} °C
+            </p>
+            <p className="maxValues" id="day2Max">
+              {(weatherDetails.list[0].main.temp_max - 273.15).toFixed(2)} °C
+            </p>
             <p className="card-text">
-              <small className="text-dark">
-                {weatherDetails.list[0].weather[0].description}
+              <small className="text-muted">
+                Feels Like :{" "}
+                {(weatherDetails.list[0].main.feels_like - 273.15).toFixed(2)}{" "}
+                °C
               </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Wind Speed :{" "}
+                {(weatherDetails.list[0].wind.speed * 3.6).toFixed(2)}km/h
+              </small>
+            </p>
+            <p className="card-text">
+              <strong className="text-dark text-bold">
+                {weatherDetails.list[0].weather[0].description}
+              </strong>
             </p>
           </div>
         </div>
@@ -73,15 +108,51 @@ const Forecast = ({ weatherDetails }) => {
                 {weatherDetails.list[8].dt_txt}
               </small>
             </p>
+            <p className="card-text">
+              <small className="">
+                Sunrise :{" "}
+                {new Date(weatherDetails.city.sunrise).toLocaleTimeString()}
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Sunset :{" "}
+                {new Date(weatherDetails.city.sunset).toLocaleTimeString()}
+              </small>
+            </p>
             <h2 className="">
-              <TiWeatherPartlySunny />
+              <img
+                src={`http://openweathermap.org/img/w/${weatherDetails.list[8].weather[0].icon}.png`}
+                alt="wthr img"
+              />
+
+              {/* <TiWeatherPartlySunny /> */}
             </h2>
             <h3>{(weatherDetails.list[8].main.temp - 273.15).toFixed(2)} °C</h3>
+            <p className="minValues" id="day2Min">
+              {(weatherDetails.list[8].main.temp_min - 273.15).toFixed(2)} °C
+            </p>
+            <p className="maxValues" id="day2Max">
+              {(weatherDetails.list[8].main.temp_max - 273.15).toFixed(2)} °C
+            </p>
             <p className="card-text">
-              <small className="text-dark">
+              <small className="text-muted">
+                Feels Like :{" "}
+                {(weatherDetails.list[8].main.feels_like - 273.15).toFixed(2)}{" "}
+                °C
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Wind Speed :{" "}
+                {(weatherDetails.list[8].wind.speed * 3.6).toFixed(2)}km/h
+              </small>
+            </p>
+            <p className="card-text">
+              <strong className="text-dark">
                 {" "}
                 {weatherDetails.list[8].weather[0].description}
-              </small>
+              </strong>
             </p>
           </div>
         </div>
@@ -93,17 +164,53 @@ const Forecast = ({ weatherDetails }) => {
                 {weatherDetails.list[16].dt_txt}
               </small>
             </p>
+            <p className="card-text">
+              <small className="">
+                Sunrise :{" "}
+                {new Date(weatherDetails.city.sunrise).toLocaleTimeString()}
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Sunset :{" "}
+                {new Date(weatherDetails.city.sunset).toLocaleTimeString()}
+              </small>
+            </p>
             <h2 className="">
-              <TiWeatherShower />
+              <img
+                src={`http://openweathermap.org/img/w/${weatherDetails.list[16].weather[0].icon}.png`}
+                alt="wthr img"
+              />
+
+              {/* <TiWeatherShower /> */}
             </h2>
             <h3>
               {(weatherDetails.list[16].main.temp - 273.15).toFixed(2)} °C
             </h3>
+            <p className="minValues" id="day2Min">
+              {(weatherDetails.list[16].main.temp_min - 273.15).toFixed(2)} °C
+            </p>
+            <p className="maxValues" id="day2Max">
+              {(weatherDetails.list[16].main.temp_max - 273.15).toFixed(2)} °C
+            </p>
             <p className="card-text">
-              <small className="text-dark">
+              <small className="text-muted">
+                Feels Like :{" "}
+                {(weatherDetails.list[16].main.feels_like - 273.15).toFixed(2)}{" "}
+                °C
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Wind Speed :{" "}
+                {(weatherDetails.list[16].wind.speed * 3.6).toFixed(2)}km/h
+              </small>
+            </p>
+            <p className="card-text">
+              <strong className="text-dark">
                 {" "}
                 {weatherDetails.list[16].weather[0].description}
-              </small>
+              </strong>
             </p>
           </div>
         </div>
@@ -115,17 +222,53 @@ const Forecast = ({ weatherDetails }) => {
                 {weatherDetails.list[24].dt_txt}
               </small>
             </p>
+            <p className="card-text">
+              <small className="">
+                Sunrise :{" "}
+                {new Date(weatherDetails.city.sunrise).toLocaleTimeString()}
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Sunset :{" "}
+                {new Date(weatherDetails.city.sunset).toLocaleTimeString()}
+              </small>
+            </p>
             <h2 className="">
-              <TiWeatherCloudy />
+              <img
+                src={`http://openweathermap.org/img/w/${weatherDetails.list[24].weather[0].icon}.png`}
+                alt="wthr img"
+              />
+
+              {/* <TiWeatherCloudy /> */}
             </h2>
             <h3>
               {(weatherDetails.list[24].main.temp - 273.15).toFixed(2)} °C
             </h3>
+            <p className="minValues" id="day2Min">
+              {(weatherDetails.list[24].main.temp_min - 273.15).toFixed(2)} °C
+            </p>
+            <p className="maxValues" id="day2Max">
+              {(weatherDetails.list[24].main.temp_max - 273.15).toFixed(2)} °C
+            </p>
             <p className="card-text">
-              <small className="text-dark">
+              <small className="text-muted">
+                Feels Like :{" "}
+                {(weatherDetails.list[24].main.feels_like - 273.15).toFixed(2)}{" "}
+                °C
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Wind Speed :{" "}
+                {(weatherDetails.list[24].wind.speed * 3.6).toFixed(2)}km/h
+              </small>
+            </p>
+            <p className="card-text">
+              <strong className="text-dark">
                 {" "}
                 {weatherDetails.list[24].weather[0].description}
-              </small>
+              </strong>
             </p>
           </div>
         </div>
@@ -137,17 +280,53 @@ const Forecast = ({ weatherDetails }) => {
                 {weatherDetails.list[32].dt_txt}
               </small>
             </p>
+            <p className="card-text">
+              <small className="">
+                Sunrise :{" "}
+                {new Date(weatherDetails.city.sunrise).toLocaleTimeString()}
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Sunset :{" "}
+                {new Date(weatherDetails.city.sunset).toLocaleTimeString()}
+              </small>
+            </p>
             <h2 className="">
-              <TiWeatherDownpour />
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherDetails.list[32].weather[0].icon}.png`}
+                alt="img"
+              />
+
+              {/* <TiWeatherDownpour /> */}
             </h2>
             <h3>
               {(weatherDetails.list[32].main.temp - 273.15).toFixed(2)} °C
             </h3>
+            <p className="minValues" id="day2Min">
+              {(weatherDetails.list[32].main.temp_min - 273.15).toFixed(2)} °C
+            </p>
+            <p className="maxValues" id="day2Max">
+              {(weatherDetails.list[32].main.temp_max - 273.15).toFixed(2)} °C
+            </p>
             <p className="card-text">
-              <small className="text-dark">
+              <small className="text-muted">
+                Feels Like :{" "}
+                {(weatherDetails.list[32].main.feels_like - 273.15).toFixed(2)}{" "}
+                °C
+              </small>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Wind Speed :{" "}
+                {(weatherDetails.list[32].wind.speed * 3.6).toFixed(2)}km/h
+              </small>
+            </p>
+            <p className="card-text">
+              <strong className="text-dark">
                 {" "}
                 {weatherDetails.list[32].weather[0].description}
-              </small>
+              </strong>
             </p>
           </div>
         </div>
